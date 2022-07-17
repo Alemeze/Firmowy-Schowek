b4270793bbfa3eede4ef6bc73bd58acac6559930
Alemeze <97557209+Alemeze@users.noreply.github.com>
2022-07-18 00:29:15 +0200

    Add files via upload

---
+++ EmbeddedMvc/Models/SchowekContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+
+namespace EmbeddedMvc.Models
+{
+    public class SchowekContext : System.Data.Entity.DbContext
+    {
+       
+    }
+}
---
+++ EmbeddedMvc/Models/SchowekModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+
+namespace EmbeddedMvc.Models
+{
+    public class SchowekModel
+    {
+        public int SchwId { get; set; }
+
+        [System.ComponentModel.DisplayName("Nazwa Schowka")]
+        public string Name { get; set; }
+
+        [System.ComponentModel.DisplayName("Treść Schowka")]
+        public string Description { get; set; }
+
+
+        [System.ComponentModel.DisplayName("Wykonano")]
+        public bool Done { get; set; }
+
+        internal SchowekModel FirstOrDefault(Func<object, bool> p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
